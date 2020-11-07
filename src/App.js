4d28f39ee54ec911e3a5589b7cb3229e388975f3
Rafal Borowski <rafall.borowski@gmail.com>
2020-11-07 18:14:19 +0100

    products section component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
 import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading="Chose your favorite" data={productData} />
+      <Products heading="Sweet Treats for You" data={productDataTwo} />
     </Router>
   );
 }
